613314fb35378876b910bdc6af97b604d320ac22
ditovermelho <carlosdanielferreira246@gmail.com>
2024-07-08 16:45:22 -0300

    aula do dia 08/07/24

--- Projeto Agenda/src/controllers/loginController.js
+++ Projeto Agenda/src/controllers/loginController.js
@@ -1,7 +1,8 @@
 const Login = require('../models/LoginModel');
 
 exports.index = (req, res) => {
-    res.render('login');
+    if(req.session.user) return res.render('login-on');
+    return res.render('login');
 };
 
 exports.register = async function (req, res) {
@@ -27,3 +28,33 @@ exports.register = async function (req, res) {
     }
 
 };
+
+exports.login = async function (req, res) {
+    try {
+        const login = new Login(req.body);
+        await login.login();
+
+        if (login.errors.length > 0) {
+            req.flash('errors', login.errors);
+            req.session.save(function () {
+                return res.redirect('back');
+            });
+            return;
+        }
+
+        req.flash('success', "Login realizado com sucesso.");
+        req.session.user = login.user;
+        req.session.save(function () {
+            return res.redirect('back');
+        });
+    } catch (e) {
+        console.log(e);
+        return res.render('404');
+    }
+
+};
+
+exports.logout = async function (req, res) {
+    req.session.destroy();
+    res.redirect('/');
+}
--- Projeto Agenda/src/models/LoginModel.js
+++ Projeto Agenda/src/models/LoginModel.js
@@ -1,5 +1,6 @@
 const mongoose = require('mongoose');
 const validator = require('validator');
+const bcryptjs = require('bcryptjs');
 
 const loginSchema = new mongoose.Schema({
     email: { type: String, required: true },
@@ -8,22 +9,50 @@ const loginSchema = new mongoose.Schema({
 
 const loginModel = mongoose.model('login', loginSchema);
 
-class login {
+class Login {
     constructor(body) {
         this.body = body;
         this.errors = [];
         this.user = null;
     }
 
-    async register() {
+    async login(){
         this.validete();
-        if (this.errorsS) return;
-        try {
-            this.user = await loginModel.create(this.body);
-        } catch (e) {
-            console.log(e);
+        if (this.errors.length > 0) return;
+
+        this.user = await loginModel.findOne({email: this.body.email});
+
+        if (!this.user) {
+            this.errors.push('Usuário não existe.');
+            return;
         }
 
+        if(!bcryptjs.compareSync(this.body.passwoed, this.user.passwoed)){
+            this.errors.push('Senha inválida.');
+            this.user = null;
+            return;
+        }
+    }
+
+    async register() {
+        this.validete();
+        if (this.errors.length > 0) return;
+
+        await this.userExists();
+
+        if (this.errors.length > 0) return;
+
+        const salt = bcryptjs.genSaltSync();
+        this.body.passwoed = bcryptjs.hashSync(this.body.passwoed, salt);
+
+        this.user = await loginModel.create(this.body);
+
+    }
+
+    async userExists(){
+        this.user = await loginModel.findOne({email: this.body.email});
+
+        if (this.user) this.errors.push('Usuário já existe.');
     }
 
     validete() {
@@ -46,10 +75,10 @@ class login {
         }
 
         this.body = {
-            email: this.body.registerInputEmail,
-            passwoed: this.body.registerInputPassword1
+            email: this.body.registerInputEmail || this.body.loginInputEmail,
+            passwoed: this.body.registerInputPassword1 || this.body.loginInputPassword
         }
     }
 }
 
-module.exports = login;
+module.exports = Login;
