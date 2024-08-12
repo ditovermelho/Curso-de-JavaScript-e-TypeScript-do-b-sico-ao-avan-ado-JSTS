fd60a3eb870ef87e95b88cf5224fbebdbbcf04c6
ditovermelho <carlosdanielferreira246@gmail.com>
2024-08-12 16:58:47 -0300

    aula do dia 12/08/24

---
+++ React_JS_Básico/listatarefas/src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './App.css';
+import Main from './components/Main.js';
+
+export default function App() {
+  return (
+    <React.StrictMode>
+      <Main />
+    </React.StrictMode>
+  );
+}
+
---
+++ React_JS_Básico/listatarefas/src/components/Main.js
@@ -0,0 +1,65 @@
+import React, { Component } from "react";
+import { FaPlus } from 'react-icons/fa';
+import { FaEdit, FaWindowClose } from "react-icons/fa";
+import './Main.css';
+
+export default class Main extends Component {
+  state = {
+    novaTarefa: '',
+    tarefas: [],
+  };
+
+  handleSubmit = (e) =>{
+    e.preventDefault();
+
+    const { tarefas } = this.state;
+    let { novaTarefa } = this.state;
+
+    novaTarefa = novaTarefa.trim();
+
+    if(tarefas.indexOf(novaTarefa) !== -1) return;
+
+    const novaTarefas = [...tarefas];
+
+    this.setState({
+      tarefas: [...novaTarefas, novaTarefa],
+    });
+  }
+
+  handleChange = (e) => {
+    this.setState({
+      novaTarefa: e.target.value,
+    });
+  };
+
+  render() {
+    const { novaTarefa, tarefas } = this.state;
+    return (
+      <div className="main">
+        <h1>Lista de tarefas</h1>
+        <p>{novaTarefa}</p>
+        <form onSubmit={this.handleSubmit} action="#" className="form">
+          <input
+            onChange={this.handleChange}
+            type="text"
+            value={novaTarefa}
+          />
+          <button type="submit">
+            <FaPlus />
+          </button>
+        </form>
+        <ul className="tarefas">
+          {tarefas.map(tarefa => (
+            <li key={tarefa}>
+              {tarefa}
+              <span>
+                <FaEdit className="edit" />
+                <FaWindowClose className="delete" />
+              </span>
+            </li>
+          ))}
+        </ul>
+      </div>
+    );
+  };
+}
