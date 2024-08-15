2411237fcdaedcc9e1e5998ca4edd7a50a1282e4
ditovermelho <carlosdanielferreira246@gmail.com>
2024-08-15 14:46:13 -0300

    aula do dia 14/08/24

--- Api Rest/src/app.js
+++ Api Rest/src/app.js
@@ -1,16 +1,34 @@
-//import dotenv from "dotenv";
+import dotenv from "dotenv";
 import { resolve } from "path";
 
-//dotenv.config();
+dotenv.config();
 
 import './database';
 
 import express from "express";
+import cors from 'cors';
+import helmet from 'helmet';
+
 import homeRoutes from './routes/homeRoutes';
 import userRoutes from './routes/userRoutes';
 import tokenRoutes from './routes/tokenRoutes';
 import alunoRoutes from './routes/alunoRoutes';
 import fotoRoutes from './routes/fotoRoutes';
+
+const whiteList = [
+  'http://localhost:3000',
+  'http://localhost:3001',
+];
+
+const corsOptions ={
+  origin: function(origin, callback){
+    if(whiteList.indexOf(origin) !== -1 || !origin){
+      callback(null, true);
+    }else{
+      callback(new Error("Not allowed by CORS"));
+    }
+  }
+}
 
 
 class App {
@@ -20,6 +38,8 @@ class App {
     this.routes();
   }
   middlewares() {
+    this.app.use(cors(corsOptions));
+    this.app.use(helmet());
     this.app.use(express.urlencoded({ extended: true }));
     this.app.use(express.json());
     this.app.use(express.static(resolve(__dirname, 'uploads')));
