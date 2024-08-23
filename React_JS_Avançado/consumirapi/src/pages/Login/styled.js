01a44388349fb4bf0c207c36a7e1a424908114e1
ditovermelho <carlosdanielferreira246@gmail.com>
2024-08-23 16:48:20 -0300

    Aula do dia 23/08/24

--- React_JS_Avançado/consumirapi/src/pages/Login/styled.js
+++ React_JS_Avançado/consumirapi/src/pages/Login/styled.js
@@ -1,5 +1,23 @@
 import styled from 'styled-components';
 
-export const Paragrafo = styled.p`
-font-size: 80px;
+import * as colors from "../../config/colors";
+
+export const Form = styled.form`
+margin-top: 20px;
+display: flex;
+flex-direction: column;
+
+input{
+  margin-bottom: 20px;
+  height: 40px;
+  font-size: 18px;
+  border: 1px solid #ddd;
+  padding: 0 10px;
+  border-radius: 4px;
+  margin-top: 5px;
+
+  &:focus{
+    border: 1px solid ${colors.primaryColor};
+  }
+}
 `;
--- React_JS_Avançado/consumirapi/src/pages/Register/styled.js
+++ React_JS_Avançado/consumirapi/src/pages/Register/styled.js
@@ -1,4 +1,5 @@
 import styled from 'styled-components';
+
 import * as colors from "../../config/colors";
 
 export const Form = styled.form`
