9421f29943fc02eb9ee835bcee0d0b0b0006243a
ditovermelho <carlosdanielferreira246@gmail.com>
2024-09-18 16:47:50 -0300

    Aula do dia 18/09/24

--- TypeScript-POO/src/A0034-interfaces/A0034-interfaces.ts
+++ TypeScript-POO/src/A0034-interfaces/A0034-interfaces.ts
@@ -1,3 +1,25 @@
+/* Interfaces
+São uma parte fundamental do TypeScript e são usadas para definir contratos ou formas que os objetos devem seguir.
+
+Definição:
+Uma interface é uma estrutura que descreve a forma de um objeto, especificando quais propriedades e métodos ele deve ter.
+Ela não gera código JavaScript; é apenas uma verificação de tipo durante a compilação.
+
+Propriedades:
+Você pode definir propriedades obrigatórias ou opcionais em uma interface.
+
+Métodos:
+Interfaces também podem definir métodos que um objeto deve implementar.
+
+Implementação:
+Quando uma classe ou objeto implementa uma interface, ele deve seguir exatamente a estrutura definida pela interface.
+
+Extensão:
+Interfaces podem estender outras interfaces, permitindo a composição de funcionalidades.
+
+Flexibilidade:
+Interfaces são flexíveis e podem ser usadas para descrever objetos, classes, funções e até mesmo tipos genéricos.
+*/
 interface TipoNome {
   nome: string;
 }
@@ -11,10 +33,10 @@ interface TipoNomeCompleto {
 }
 
 type TipoPessoa = TipoNome & TipoSobrenome & TipoNomeCompleto;
-interface TipoPessoa2 extends TipoNome, TipoSobrenome, TipoNomeCompleto {}
+interface TipoPessoa2 extends TipoNome, TipoSobrenome, TipoNomeCompleto { }
 
 export class Pessoa implements TipoPessoa2 {
-  constructor(public nome: string, public sobrenome: string) {}
+  constructor(public nome: string, public sobrenome: string) { }
 
   nomeCompleto(): string {
     return this.nome + ' ' + this.sobrenome;
